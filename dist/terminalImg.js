3e7f29e3a5c2a360754a95adaaddfeb78c0e4cd2
Golo Roden <golo.roden@thenativeweb.io>
2018-07-07 12:49:19 +0200

    Release 1.1.0.

--- dist/terminalImg.js
+++ dist/terminalImg.js
@@ -16,11 +16,11 @@ var chalk = require('chalk'),
 var characterFullBlock = '\u2588',
     characterLowerHalfBlock = '\u2584';
 
-var draw = function () {
+var drawAsString = function () {
   var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(file, _ref) {
     var width = _ref.width,
         height = _ref.height;
-    var png, requestedWidth, requestedHeight, resizedLogo, y, x, upperColor, lowerColor;
+    var image, requestedWidth, requestedHeight, resizedImage, result, y, x, upperColor, lowerColor;
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
@@ -42,55 +42,59 @@ var draw = function () {
             return Jimp.read(file);
 
           case 5:
-            png = _context.sent;
+            image = _context.sent;
             requestedWidth = width || Jimp.AUTO;
             requestedHeight = height || Jimp.AUTO;
-            resizedLogo = png.resize(requestedWidth, requestedHeight, Jimp.RESIZE_NEAREST_NEIGHBOR);
+            resizedImage = image.resize(requestedWidth, requestedHeight, Jimp.RESIZE_NEAREST_NEIGHBOR);
+            result = '';
             y = 0;
 
-          case 10:
-            if (!(y < resizedLogo.bitmap.height)) {
-              _context.next = 26;
+          case 11:
+            if (!(y < resizedImage.bitmap.height)) {
+              _context.next = 27;
               break;
             }
 
             x = 0;
 
-          case 12:
-            if (!(x < resizedLogo.bitmap.width)) {
-              _context.next = 22;
+          case 13:
+            if (!(x < resizedImage.bitmap.width)) {
+              _context.next = 23;
               break;
             }
 
-            upperColor = resizedLogo.getPixelColor(x, y);
-            lowerColor = resizedLogo.getPixelColor(x, y + 1);
+            upperColor = resizedImage.getPixelColor(x, y);
+            lowerColor = resizedImage.getPixelColor(x, y + 1);
 
             if (!(upperColor === lowerColor)) {
-              _context.next = 18;
+              _context.next = 19;
               break;
             }
 
-            process.stdout.write(chalk.bgHex(upperColor).hex(upperColor)(characterFullBlock));
-            return _context.abrupt('continue', 19);
-
-          case 18:
-
-            process.stdout.write(chalk.bgHex(upperColor).hex(lowerColor)(characterLowerHalfBlock));
+            result += chalk.bgHex(upperColor).hex(upperColor)(characterFullBlock);
+            return _context.abrupt('continue', 20);
 
           case 19:
-            x++;
-            _context.next = 12;
-            break;
 
-          case 22:
-            process.stdout.write('\n');
+            result += chalk.bgHex(upperColor).hex(lowerColor)(characterLowerHalfBlock);
+
+          case 20:
+            x++;
+            _context.next = 13;
+            break;
 
           case 23:
+            result += '\n';
+
+          case 24:
             y += 2;
-            _context.next = 10;
+            _context.next = 11;
             break;
 
-          case 26:
+          case 27:
+            return _context.abrupt('return', result);
+
+          case 28:
           case 'end':
             return _context.stop();
         }
@@ -98,9 +102,41 @@ var draw = function () {
     }, _callee, this);
   }));
 
-  return function draw(_x, _x2) {
+  return function drawAsString(_x, _x2) {
     return _ref2.apply(this, arguments);
   };
 }();
+
+var draw = function () {
+  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(file, _ref3) {
+    var width = _ref3.width,
+        height = _ref3.height;
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.t0 = process.stdout;
+            _context2.next = 3;
+            return drawAsString(file, { width: width, height: height });
+
+          case 3:
+            _context2.t1 = _context2.sent;
+
+            _context2.t0.write.call(_context2.t0, _context2.t1);
+
+          case 5:
+          case 'end':
+            return _context2.stop();
+        }
+      }
+    }, _callee2, this);
+  }));
+
+  return function draw(_x3, _x4) {
+    return _ref4.apply(this, arguments);
+  };
+}();
+
+draw.asString = drawAsString;
 
 module.exports = draw;
