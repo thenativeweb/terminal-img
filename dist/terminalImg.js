11c932781cb08218aa1355d0cdc57416a054c180
Golo Roden <golo.roden@thenativeweb.io>
2019-02-02 15:15:29 +0100

    Release 1.1.1.

--- dist/terminalImg.js
+++ dist/terminalImg.js
@@ -1,47 +1,46 @@
 'use strict';
 
-var _regenerator = require('babel-runtime/regenerator');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _regenerator2 = _interopRequireDefault(_regenerator);
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
 var chalk = require('chalk'),
     Jimp = require('jimp');
 
-var characterFullBlock = '\u2588',
-    characterLowerHalfBlock = '\u2584';
+var characterFullBlock = "\u2588",
+    characterLowerHalfBlock = "\u2584";
 
-var drawAsString = function () {
-  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(file, _ref) {
-    var width = _ref.width,
-        height = _ref.height;
-    var image, requestedWidth, requestedHeight, resizedImage, result, y, x, upperColor, lowerColor;
-    return _regenerator2.default.wrap(function _callee$(_context) {
+var drawAsString =
+/*#__PURE__*/
+function () {
+  var _ref2 = (0, _asyncToGenerator2.default)(
+  /*#__PURE__*/
+  _regenerator.default.mark(function _callee(file, _ref) {
+    var width, height, image, requestedWidth, requestedHeight, resizedImage, result, y, x, upperColor, lowerColor;
+    return _regenerator.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
+            width = _ref.width, height = _ref.height;
+
             if (file) {
-              _context.next = 2;
+              _context.next = 3;
               break;
             }
 
             throw new Error('File is missing.');
 
-          case 2:
-
+          case 3:
             if (!width && !height) {
               width = process.stdout.columns || 80;
             }
 
-            _context.next = 5;
+            _context.next = 6;
             return Jimp.read(file);
 
-          case 5:
+          case 6:
             image = _context.sent;
             requestedWidth = width || Jimp.AUTO;
             requestedHeight = height || Jimp.AUTO;
@@ -49,17 +48,17 @@ var drawAsString = function () {
             result = '';
             y = 0;
 
-          case 11:
+          case 12:
             if (!(y < resizedImage.bitmap.height)) {
-              _context.next = 27;
+              _context.next = 28;
               break;
             }
 
             x = 0;
 
-          case 13:
+          case 14:
             if (!(x < resizedImage.bitmap.width)) {
-              _context.next = 23;
+              _context.next = 24;
               break;
             }
 
@@ -67,35 +66,34 @@ var drawAsString = function () {
             lowerColor = resizedImage.getPixelColor(x, y + 1);
 
             if (!(upperColor === lowerColor)) {
-              _context.next = 19;
+              _context.next = 20;
               break;
             }
 
             result += chalk.bgHex(upperColor).hex(upperColor)(characterFullBlock);
-            return _context.abrupt('continue', 20);
-
-          case 19:
-
-            result += chalk.bgHex(upperColor).hex(lowerColor)(characterLowerHalfBlock);
+            return _context.abrupt("continue", 21);
 
           case 20:
-            x++;
-            _context.next = 13;
-            break;
+            result += chalk.bgHex(upperColor).hex(lowerColor)(characterLowerHalfBlock);
 
-          case 23:
-            result += '\n';
+          case 21:
+            x++;
+            _context.next = 14;
+            break;
 
           case 24:
+            result += '\n';
+
+          case 25:
             y += 2;
-            _context.next = 11;
+            _context.next = 12;
             break;
 
-          case 27:
-            return _context.abrupt('return', result);
-
           case 28:
-          case 'end':
+            return _context.abrupt("return", result);
+
+          case 29:
+          case "end":
             return _context.stop();
         }
       }
@@ -107,25 +105,32 @@ var drawAsString = function () {
   };
 }();
 
-var draw = function () {
-  var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(file, _ref3) {
-    var width = _ref3.width,
-        height = _ref3.height;
-    return _regenerator2.default.wrap(function _callee2$(_context2) {
+var draw =
+/*#__PURE__*/
+function () {
+  var _ref4 = (0, _asyncToGenerator2.default)(
+  /*#__PURE__*/
+  _regenerator.default.mark(function _callee2(file, _ref3) {
+    var width, height;
+    return _regenerator.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
+            width = _ref3.width, height = _ref3.height;
             _context2.t0 = process.stdout;
-            _context2.next = 3;
-            return drawAsString(file, { width: width, height: height });
+            _context2.next = 4;
+            return drawAsString(file, {
+              width: width,
+              height: height
+            });
 
-          case 3:
+          case 4:
             _context2.t1 = _context2.sent;
 
             _context2.t0.write.call(_context2.t0, _context2.t1);
 
-          case 5:
-          case 'end':
+          case 6:
+          case "end":
             return _context2.stop();
         }
       }
@@ -138,5 +143,4 @@ var draw = function () {
 }();
 
 draw.asString = drawAsString;
-
 module.exports = draw;
